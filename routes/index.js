4987d852de3703a4fc85be16e9e2781229de73a7
pohjie <0xArlet@gmail.com>
2023-03-07 11:54:00 +0800

    Allow user to submit their message

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,37 @@
 const messages = [
   {
-    text: 'Hi there!',
-    user: 'Amanda',
-    added: new Date()
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date(),
   },
   {
-    text: 'Hello World!',
-    user: 'Charles',
-    added: new Date()
-  }
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
 ];
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+/* GET form page */
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+/* POST form page */
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
